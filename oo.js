689bd231438ec7339ec5ab21753e0b257aa31876
Henrique dos Santos Goulart <henriquedsg89@gmail.com>
2013-10-06 17:41:09 -0300

    Adicionado um método ao protótipo pessoa

--- oo.js
+++ oo.js
@@ -1,6 +1,9 @@
 function Person(name, age) {
 	this.name = name;
 	this.age = age;
+	this.sayHello = function() {
+	     alert("My name is " + this.name + ", and i'm " + this.age + " years old");
+	};
 }
 
 var henrique = new Person("Henrique", 65);
@@ -8,3 +11,5 @@ var octavio = new Person("Octavio", 57);
 
 alert("This script was created by " + henrique.name + " - " + octavio.name);
 
+henrique.sayHello();
+octavio.sayHello();
